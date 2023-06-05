41b9942f813d5e2ac4f3ab4f9868987ed2a9934c
Ian Tester <imroykun@gmail.com>
2023-06-05 15:01:03 +1000

    add start of PNG format reader
    Need to implement reading chunk types other than IHDR and checking chunk CRC

---
+++ src/chunks.rs
@@ -0,0 +1,517 @@
+/*
+  png-container
+  Copyright (C) 2023 Ian Tester
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*! PNG chunks
+ */
+
+use std::io::{Read, Seek, SeekFrom};
+use std::str;
+
+/// Palette entry
+#[derive(Clone, Debug)]
+pub struct PNGPaletteEntry {
+    red: u8,
+    green: u8,
+    blue: u8,
+
+}
+
+
+/// Colour type of image
+#[derive(Copy, Clone, Debug)]
+pub enum PNGColourType {
+    /// Greyscale image - allowed depths of 1, 2, 4, 8, or 16 bits per component
+    Greyscale = 0,
+
+    /// RGB colour image - allowed depths of 8 or 16 bits per component
+    TrueColour = 2,
+
+    /// Indexed colour image - allowed depths of 1, 2, 4, or 8 bits per index
+    IndexedColour,
+
+    /// Greyscale image with alpha - allowed depths of 8 or 16 bits per component
+    GreyscaleAlpha,
+
+    /// RGB colour image with alpha - allowed depths of 8 or 16 bits per component
+    TrueColourAlpha = 6,
+
+}
+
+impl TryFrom<u8> for PNGColourType {
+    type Error = std::io::Error;
+    fn try_from(val: u8) -> Result<Self, Self::Error> {
+        match val {
+            x if x == PNGColourType::Greyscale as u8 => Ok(PNGColourType::Greyscale),
+            x if x == PNGColourType::TrueColour as u8 => Ok(PNGColourType::TrueColour),
+            x if x == PNGColourType::IndexedColour as u8 => Ok(PNGColourType::IndexedColour),
+            x if x == PNGColourType::GreyscaleAlpha as u8 => Ok(PNGColourType::GreyscaleAlpha),
+            x if x == PNGColourType::TrueColourAlpha as u8 => Ok(PNGColourType::TrueColourAlpha),
+            _ => Err(std::io::Error::other(format!("PNG: Invalid value of colour type ({})", val))),
+        }
+    }
+}
+
+
+/// Compression type(s)
+#[derive(Copy, Clone, Debug)]
+pub enum PNGCompressionType {
+    /// DEFLATE
+    Zlib = 0,
+
+}
+
+impl TryFrom<u8> for PNGCompressionType {
+    type Error = std::io::Error;
+    fn try_from(val: u8) -> Result<Self, Self::Error> {
+        match val {
+            x if x == PNGCompressionType::Zlib as u8 => Ok(PNGCompressionType::Zlib),
+            _ => Err(std::io::Error::other(format!("PNG: Invalid value of compression method ({})", val))),
+        }
+    }
+}
+
+
+/// Filter types
+#[derive(Copy, Clone, Debug)]
+pub enum PNGFilterType {
+    /// Adaptive filtering with five basic filter types
+    Adaptive = 0,
+
+}
+
+impl TryFrom<u8> for PNGFilterType {
+    type Error = std::io::Error;
+    fn try_from(val: u8) -> Result<Self, Self::Error> {
+        match val {
+            x if x == PNGFilterType::Adaptive as u8 => Ok(PNGFilterType::Adaptive),
+            _ => Err(std::io::Error::other(format!("PNG: Invalid value of filter method ({})", val))),
+        }
+    }
+}
+
+
+/// Interlacing types
+#[derive(Copy, Clone, Debug)]
+pub enum PNGInterlaceType {
+    /// No interlacing
+    None = 0,
+
+    /// Adam7 interlacing
+    Adam7,
+
+}
+
+impl TryFrom<u8> for PNGInterlaceType {
+    type Error = std::io::Error;
+    fn try_from(val: u8) -> Result<Self, Self::Error> {
+        match val {
+            x if x == PNGInterlaceType::None as u8 => Ok(PNGInterlaceType::None),
+            x if x == PNGInterlaceType::Adam7 as u8 => Ok(PNGInterlaceType::Adam7),
+            _ => Err(std::io::Error::other(format!("PNG: Invalid value of interlace method ({})", val))),
+        }
+    }
+}
+
+
+/// Contents of tRNS chunk
+#[derive(Clone, Debug)]
+pub enum PNGtRNSType {
+    Greyscale {
+        value: u16,
+    },
+
+    TrueColour {
+        red: u16,
+        green: u16,
+        blue: u16,
+    },
+
+    IndexedColour {
+        values: Vec<u8>,
+    },
+
+}
+
+
+/// Contents of sBIT chunk
+#[derive(Copy, Clone, Debug)]
+pub enum PNGsBITType {
+    Greyscale {
+        grey_bits: u8,
+    },
+
+    Colour {
+        red_bits: u8,
+        green_bits: u8,
+        blue_bits: u8,
+    },
+
+    GreyscaleAlpha {
+        grey_bits: u8,
+        alpha_bits: u8,
+    },
+
+    TrueColourAlpha {
+        red_bits: u8,
+        green_bits: u8,
+        blue_bits: u8,
+        alpha_bits: u8,
+    },
+
+}
+
+
+/// ICC rendering intent
+#[derive(Copy, Clone, Debug)]
+pub enum PNGRenderingIntent {
+    Perceptual,
+    Relative_colorimetric,
+    Saturation,
+    Absolute_colorimetric,
+}
+
+impl TryFrom<u8> for PNGRenderingIntent {
+    type Error = std::io::Error;
+    fn try_from(val: u8) -> Result<Self, Self::Error> {
+        match val {
+            x if x == PNGRenderingIntent::Perceptual as u8 => Ok(PNGRenderingIntent::Perceptual),
+            x if x == PNGRenderingIntent::Relative_colorimetric as u8 => Ok(PNGRenderingIntent::Relative_colorimetric),
+            x if x == PNGRenderingIntent::Saturation as u8 => Ok(PNGRenderingIntent::Saturation),
+            x if x == PNGRenderingIntent::Absolute_colorimetric as u8 => Ok(PNGRenderingIntent::Absolute_colorimetric),
+            _ => Err(std::io::Error::other(format!("PNG: Invalid value of rendering intent ({})", val))),
+        }
+    }
+
+}
+
+
+/// Contents of bKGD chunk
+#[derive(Copy, Clone, Debug)]
+pub enum PNGbKGDType {
+    Greyscale {
+        value: u16,
+    },
+
+    TrueColour {
+        red: u16,
+        green: u16,
+        blue: u16,
+    },
+
+    IndexedColour {
+        index: u8,
+    },
+
+}
+
+
+/// Unit type used in several chunks
+#[derive(Copy, Clone, Debug)]
+pub enum PNGUnitType {
+    Unknown = 0,
+
+    Metre = 1,
+
+}
+
+
+/// Enum of PNG chunk types and the data they hold
+#[derive(Clone, Debug)]
+pub enum PNGChunkType<'a> {
+    // Critical chunks
+
+    /// Image header
+    IHDR {
+        /// Width of image in pixels
+        width: u32,
+
+        /// Height of image in pixels
+        height: u32,
+
+        /// Number of bits per sample
+        bit_depth: u8,
+
+        /// Colour type
+        colour_type: PNGColourType,
+
+        /// Compression method
+        compression_method: PNGCompressionType,
+
+        /// Filter method
+        filter_method: PNGFilterType,
+
+        /// Interlace method
+        interlace_method: PNGInterlaceType,
+    },
+
+    /// Palette
+    PLTE {
+        entries: Vec<PNGPaletteEntry>,
+    },
+
+    /// Image data
+    IDAT {
+        data: Vec<u8>,
+    },
+
+    /// Image end
+    IEND,
+
+    // Transparency information
+
+    /// Transparency
+    TRNS {
+        data: PNGtRNSType,
+    },
+
+    // Colour space information
+
+    /// Primary chromaticities and white point
+    ///
+    /// Values are scaled by 100000
+    CHRM {
+        white_x: u32,
+        white_y: u32,
+        red_x: u32,
+        red_y: u32,
+        green_x: u32,
+        green_y: u32,
+        blue_x: u32,
+        blue_y: u32,
+    },
+
+    /// Image gamma
+    ///
+    /// Value is scaled by 100000
+    GAMA {
+        gamma: u32,
+    },
+
+    /// Embedded ICC profile
+    ICCP {
+        name: &'a str,
+        compression_method: PNGCompressionType,
+        compressed_profile: Vec<u8>,
+    },
+
+    /// Significant bits
+    SBIT {
+        bits: PNGsBITType,
+    },
+
+    /// Standard RGB colour space
+    SRGB {
+        rendering_intent: PNGRenderingIntent,
+    },
+
+    /// Coding-independent code points for video signal type identification
+    CICP {
+        colour_primaries: u8,
+        transfer_function: u8,
+        matrix_coeffs: u8,
+        video_full_range: bool,
+    },
+
+    // Textual information
+
+    /// Textual data
+    TEXT {
+        keyword: &'a str,
+        string: &'a str,
+    },
+
+    /// Compressed textual data
+    ZTXT {
+        keyword: &'a str,
+        compression_method: PNGCompressionType,
+        compressed_string: Vec<u8>,
+    },
+
+    /// International textual data
+    ITXT {
+        keyword: &'a str,
+        compressed: bool,
+        compression_method: PNGCompressionType,
+        language: &'a str,
+        translated_keyword: &'a str,
+        compressed_string: Vec<u8>,
+    },
+
+    // Miscellaneous information
+
+    /// Background colour
+    BKGD {
+        data: PNGbKGDType,
+    },
+
+    /// Image histogram
+    HIST {
+        frequencies: Vec<u16>,
+    },
+
+    /// Physical pixel dimensions
+    PHYS {
+        x_pixels_per_unit: u32,
+        y_pixels_per_unit: u32,
+        unit: PNGUnitType,
+    },
+
+    /// Suggested palette
+    SPLT {
+        name: &'a str,
+        depth: u8,
+        // TODO
+    },
+
+    /// Exchangeable Image File (Exif) Profile
+    EXIF {
+        profile: Vec<u8>,
+    },
+
+    // Time stamp information
+
+    /// Image last-modification time
+    TIME {
+        year: u16,
+        month: u8,
+        day: u8,
+        hour: u8,
+        minute: u8,
+        second: u8,
+    },
+
+    // Animation information
+
+    // TODO
+
+    // Extensions
+
+    /// Image offset
+    OFFS {
+        x: u32,
+        y: u32,
+        unit: PNGUnitType,
+    },
+
+    /// Calibration of pixel values
+    PCAL {
+        name: &'a str,
+        original_zero: u32,
+        original_max: u32,
+        equation_type: u8,
+        num_parameters: u8,
+        unit_name: &'a str,
+        parameters: Vec<&'a str>,
+    },
+
+    /// Physical scale of image subject
+    SCAL {
+        unit: PNGUnitType,
+        pixel_width: &'a str,
+        pixel_height: &'a str,
+    },
+
+    /// GIF Graphic Control Extension
+    GIFG {
+        disposal_method: u8,
+        user_input: bool,
+        delay_time: u16,
+    },
+
+    /// GIF Application Extension
+    GIFX {
+        app_id: [ u8; 8 ],
+        app_code: [ u8; 3 ],
+        app_data: Vec<u8>,
+    },
+
+    /// Indicator of Stereo Image
+    STER {
+        mode: u8,
+    },
+
+}
+
+impl<'a> PNGChunkType<'a> {
+
+}
+
+
+/// A chunk in a PNG file
+#[derive(Copy, Clone, Debug)]
+pub struct PNGChunk {
+    /// The position in the stream/file for this chunk
+    pub position: u64,
+
+    pub length: u32,
+
+    pub chunktype: [ u8; 4 ],
+
+    pub crc: u32,
+
+    /// Is this chunk necessary for successful display of the contents of
+    /// the datastream (false) or not (true)? Derived from the case of the
+    /// first character of the chunk type.
+    pub ancillary: bool,
+
+    /// Is this chunk defined publically (false) or privately (true)? Derived
+    /// from the case of the second character of the chunk type.
+    pub private: bool,
+
+    /// Reserved for future use. All chunks should have this set to false.
+    /// Derived from the case of the third character of the chunk type.
+    pub reserved: bool,
+
+    /// Is this chunk safe to copy to a new datastream without processing?
+    /// Derived from the case of the fourth character of the chunk type.
+    pub safe_to_copy: bool,
+
+}
+
+// because u32::from_be_bytes() only takes fixed-length arrays and it's too
+// much of a PITA to convert from a slice.
+fn u32_be(bytes: &[u8]) -> u32 {
+    (bytes[3] as u32) | ((bytes[2] as u32) << 8) | ((bytes[1] as u32) << 16) | ((bytes[0] as u32) << 24)
+}
+
+impl PNGChunk {
+    pub fn read_chunk<'a, R>(&self, stream: &mut R) -> Result<PNGChunkType<'a>, std::io::Error>
+        where R: Read + Seek
+    {
+        stream.seek(SeekFrom::Start(self.position + 8))?;
+        let chunktypestr = str::from_utf8(&self.chunktype).unwrap();
+        match chunktypestr {
+            "IHDR" => {
+                let mut ihdr = [ 0_u8; 13 ];
+                stream.read_exact(&mut ihdr)?;
+                Ok(PNGChunkType::IHDR {
+                    width: u32_be(&ihdr[0..4]),
+                    height: u32_be(&ihdr[4..8]),
+                    bit_depth: ihdr[8],
+                    colour_type: ihdr[9].try_into()?,
+                    compression_method: ihdr[10].try_into()?,
+                    filter_method: ihdr[11].try_into()?,
+                    interlace_method: ihdr[12].try_into()?,
+                })
+            },
+
+            _ => Err(std::io::Error::other(format!("PNG: Unhandled chunk type ({})", chunktypestr)))
+        }
+    }
+
+}
---
+++ src/reader.rs
@@ -0,0 +1,158 @@
+/*
+  png-container
+  Copyright (C) 2023 Ian Tester
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*! PNG reader
+ */
+
+use std::collections::HashMap;
+use std::io::{Read, Seek, SeekFrom};
+use std::str;
+
+use crate::chunks::*;
+
+/// A PNG file reader
+#[derive(Debug)]
+pub struct PNGFileReader<R> {
+    pub width: u32,
+
+    pub height: u32,
+
+    pub bit_depth: u8,
+
+    pub colour_type: PNGColourType,
+
+    pub stream: R,
+
+    pub all_chunks: Vec<PNGChunk>,
+
+    ihdr_idx: usize,
+    iend_idx: usize,
+
+    optional_chunk_idxs: HashMap<[ u8; 4 ], usize>,
+    optional_multi_chunk_idxs: HashMap<[ u8; 4 ], Vec<usize>>,
+}
+
+impl<R> PNGFileReader<R>
+where R: Read + Seek
+{
+    /// Constructor from a Read-able type
+    fn from_stream(mut stream: R) -> Result<Self, std::io::Error> {
+        // First check the signature
+        {
+            let mut buf = [ 0; 8 ];
+            stream.read_exact(&mut buf)?;
+            if buf != [ 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a ] {
+                return Err(std::io::Error::new(std::io::ErrorKind::Other, "PNG: Bad signature"));
+            }
+        }
+
+        let mut fr = PNGFileReader {
+            width: 0,
+            height: 0,
+            bit_depth: 0,
+            colour_type: PNGColourType::Greyscale,
+            stream,
+            all_chunks: Vec::new(),
+            ihdr_idx: 0,
+            iend_idx: 0,
+            optional_chunk_idxs: HashMap::new(),
+            optional_multi_chunk_idxs: HashMap::new(),
+        };
+
+        // Now just loop reading chunks
+        loop {
+            let position = fr.stream.stream_position()?;
+
+            let mut buf4 = [ 0_u8; 4 ];
+            fr.stream.read_exact(&mut buf4)?;
+            let length = u32::from_be_bytes(buf4);
+
+            let mut chunktype = [ 0_u8; 4 ];
+            fr.stream.read_exact(&mut chunktype)?;
+            let chunktypestr = str::from_utf8(&chunktype).unwrap();
+
+            fr.stream.seek(SeekFrom::Current(length as i64))?;
+            // TODO: check CRC
+            fr.stream.read_exact(&mut buf4)?;
+            let crc = u32::from_be_bytes(buf4);
+
+            let idx = fr.all_chunks.len();
+
+            fr.all_chunks.push(PNGChunk {
+                position,
+                length,
+                chunktype,
+                crc,
+                ancillary: chunktype[0] & 0x20 > 0,
+                private: chunktype[1] & 0x20 > 0,
+                reserved: chunktype[2] & 0x20 > 0,
+                safe_to_copy: chunktype[3] & 0x20 > 0,
+            });
+
+            match chunktypestr {
+                "IHDR" => {
+                    fr.ihdr_idx = idx;
+                    let oldpos = fr.stream.stream_position()?;
+                    // Fill in image metadata
+                    let ihdr = fr.all_chunks[idx].read_chunk(&mut fr.stream)?;
+                    match ihdr {
+                        PNGChunkType::IHDR { width, height, bit_depth, colour_type, compression_method: _, filter_method: _, interlace_method: _ } => {
+                            fr.width = width;
+                            fr.height = height;
+                            fr.bit_depth = bit_depth;
+                            fr.colour_type = colour_type;
+                        },
+
+                        _ => (),
+                    }
+
+                    fr.stream.seek(SeekFrom::Start(oldpos))?;
+                },
+
+                "IEND" => {
+                    fr.iend_idx = idx;
+                    break;
+                },
+
+                "IDAT" | "fcTL" | "tEXt" | "iTXt" | "zTXt" => {
+                    if !fr.optional_multi_chunk_idxs.contains_key(&chunktype) {
+                        fr.optional_multi_chunk_idxs.insert(chunktype, Vec::new());
+                    }
+                    fr.optional_multi_chunk_idxs.get_mut(&chunktype).unwrap().push(idx);
+                },
+
+                _ => {
+                    fr.optional_chunk_idxs.insert(chunktype, idx);
+                },
+            }
+        }
+
+        Ok(fr)
+    }
+
+    /*
+    pub fn chunks(&self) -> std::slice::Iter<'_, PNGChunk> {
+        self.all_chunks.iter()
+}
+    */
+
+    pub fn ihdr(&self) -> PNGChunk {
+        self.all_chunks[self.ihdr_idx]
+    }
+
+}
